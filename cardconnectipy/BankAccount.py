866ff132d1493fa944ccce216d2d4214e7668133
root <root@kevmo314.uchicago.edu>
2014-08-03 20:08:17 -0500

    implemented BankAccount.py

--- cardconnectipy/BankAccount.py
+++ cardconnectipy/BankAccount.py
@@ -4,4 +4,39 @@ class BankAccount(PaymentMethod):
 	CHECKING = 1
 	SAVINGS = 2
 	def __init__(self, **kwargs):
-		raise NotImplementedError
+		self.bankaba = None				# routing number -- self.account is now the banking account number instead
+		PaymentMethod.__init__(self, **kwargs)
+
+	@staticmethod
+	def create(**kwargs):
+		return BankAccount(**kwargs)
+
+	# base AUTHORIZATION request
+	def auth(amount=None, bankaba=None, **kwargs):
+		if(bankaba == None):
+			bankaba = getattr(self, 'bankaba', None)
+		return super(BankAccount, self).auth(amount=amount, bankaba=bankaba, **kwargs)
+
+	def capture(amount=None, bankaba=None, **kwargs):
+		if(bankaba == None):
+			bankaba = getattr(self, 'bankaba', None)
+		return super(BankAccount, self).capture(amount=amount, bankaba=bankaba, **kwargs)
+
+	# amount to be gained by the company
+	def sale(amount=None, bankaba=None):
+		if(bankaba == None):
+			bankaba = getattr(self, 'bankaba', None)
+		return self.capture(amount=amount, bankaba=bankaba)
+
+	# amount to be sent back to user -- amount is therefore treated as NEGATIVE in AUTH request
+	# input as a POSITIVE number
+	def credit(amount=None, bankaba=None):
+		if(bankaba == None):
+			bankaba = getattr(self, 'bankaba', None)
+		return self.capture(amount=-amount, bankaba=bankaba)
+
+	# shorthand for auth(0, bankaba)
+	def verify(bankaba=None):
+		if(bankaba == None):
+			bankaba = getattr(self, 'bankaba', None)
+		return super(BankAccount, self).verify(amount=amount, bankaba=bankaba)
